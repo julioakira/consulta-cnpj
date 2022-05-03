15d337f897f64c80ec6debb1a2bc86ff4a3c9810
julioakira <julio.akira.pedroso@gmail.com>
2022-05-03 16:33:09 -0300

    KRON-02 - small changes in controller

--- src/controllers/base_controller.ts
+++ src/controllers/base_controller.ts
@@ -2,7 +2,7 @@ import express from 'express'
 
 class BaseController {
   async store(req: express.Request, res: express.Response): Promise<express.Response<string>> {
-    return res.json({ message: 'Everything okay here!' })
+    return res.json({ message: 'Everything okay LALALA!' })
   }
 }
 
